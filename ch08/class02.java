4777cc5f4fb3a474cc721da0ed25e6a10679b2fe
109b29723 <92261548+109b29723@users.noreply.github.com>
2021-10-18 11:13:17 +0800

    Update class02.java

--- ch08/class02.java
+++ ch08/class02.java
@@ -1,1 +1,26 @@
-
+package Java08;
+public class Class02 {
+	public static void main(String[] args) {
+		Cbbb obj1 = new Cbbb();
+		obj1.x = 5.2;
+		obj1.y = 3.9;
+		Cbbb obj2 = new Cbbb();
+		obj2.x = 6.5;
+		obj2.y = 4.6;
+		Cbbb avg = new Cbbb();
+		avg.x = (obj1.x + obj2.x)/2;
+		avg.y = (obj1.y + obj2.y)/2;
+		
+		System.out.println(obj1.x);
+		System.out.println(obj1.y);
+		System.out.println(obj2.x);
+		System.out.println(obj2.y);
+		System.out.println(avg.x);
+		System.out.println(avg.y);
+	}
+}
+class Cbbb
+{
+	double x;
+	double y;
+}
