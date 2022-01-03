59dc457d850ac46fc0d1f29237e3064858d39e05
109b29723 <92261548+109b29723@users.noreply.github.com>
2022-01-03 08:58:06 +0800

    Update Class14.java

--- Ch12/Class14.java
+++ Ch12/Class14.java
@@ -2,8 +2,8 @@ public class Class14
 {
    public static void main(String args[])
    {
-     String str="Habit is second nature.";
-     System.out.println("string="+str);
-     System.out.println("sub-string="+str.substring(16,22));
+     String s="Habit is second nature.";
+     String a = s.substring(16,22);
+     System.out.println(a);
    }
 }
