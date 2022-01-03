e2e4ea81f7004996de8faaa86f1a8c8d5cd3dd40
109b29723 <92261548+109b29723@users.noreply.github.com>
2022-01-03 08:28:48 +0800

    Create Class16.java

---
+++ Ch12/Class16.java
@@ -0,0 +1,10 @@
+public class Class16
+{
+   public static void main(String args[])
+   {
+     String s="262904713";
+     int n;
+     n=Integer.parseInt(s);
+     System.out.println(n*2);
+   }
+}
