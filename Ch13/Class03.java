f6c41a15f81db86a7b41fb47021610d7cfb7ae55
109b29723 <92261548+109b29723@users.noreply.github.com>
2022-01-09 18:03:50 +0800

    Create Class03.java

---
+++ Ch13/Class03.java
@@ -0,0 +1,21 @@
+// hw13_1, 例外訊息的擷取
+public class Class03
+{
+    public static void main(String args[])
+    {
+        int num=12,den=0;
+        try
+        {
+            int ans=num/den;
+        }
+        catch(ArithmeticException e)
+        {
+
+        }
+        finally
+        {
+            
+        }
+        System.out.println("end of main()!!");
+    }
+}
