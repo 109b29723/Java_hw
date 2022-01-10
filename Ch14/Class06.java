b7fec9bf0155e8a8b42025b7a4561101e11e6aea
109b29723 <92261548+109b29723@users.noreply.github.com>
2022-01-10 13:47:26 +0800

    Create Class06.java

---
+++ Ch14/Class06.java
@@ -0,0 +1,22 @@
+import java.io.*;
+public class Class06
+{
+   public static void main(String args[])
+   {
+       try
+       {
+           char data[]=new char[128];
+           FileReader fr=new FileReader("C:\\Users\\User\\Desktop\\donkey.txt");
+           
+           int num=fr.read(data);
+           String str=new String(data,0,num);
+           System.out.println("Characters read= "+num);
+           System.out.println(str);
+           
+           fr.close();
+        }
+        catch(IOException e)
+        {
+        }
+    }
+}
